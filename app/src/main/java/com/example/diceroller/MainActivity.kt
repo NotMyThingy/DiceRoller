19921682d941c73e35c819dde63e0c0b1f700079
NotMyThingy <miska.janhunen@protonmail.com>
2021-06-11 15:35:59 +0300

    lateinit : imageView

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -7,6 +7,9 @@ import android.widget.ImageView
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,12 +18,14 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener {
             rollDice()
         }
+
+        diceImage = findViewById(R.id.dice_image)
+
     }
 
     private fun rollDice() {
-        val randInt = Random.nextInt(6) + 1
-        val diceImage: ImageView = findViewById(R.id.dice_image)
-        val drawableResource = when (randInt) {
+
+        val drawableResource = when (Random.nextInt(6) + 1) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3

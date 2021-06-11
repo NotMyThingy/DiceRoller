65553aa95c87e6a6f6b9ee9371b646f5aadb6a02
NotMyThingy <miska.janhunen@protonmail.com>
2021-06-11 14:28:25 +0300

    rollDice() - method and resultText as a TextView

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,8 +14,14 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.text = "They see me rollin ..."
         rollButton.setOnClickListener {
-            Toast.makeText(this, "Did you just click me?", Toast.LENGTH_SHORT).show()
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val randInt = Random.nextInt(6) + 1
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = randInt.toString()
     }
 
 }
